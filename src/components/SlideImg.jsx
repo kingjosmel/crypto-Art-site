388d8707bc9cba929568630cb56af0dde351be5a
melvin <ianndiormel@gmail.com>
2025-02-07 13:43:24 +0100

    fix: image errors

--- src/components/SlideImg.jsx
+++ src/components/SlideImg.jsx
@@ -1,12 +1,12 @@
 import { motion } from "framer-motion";
-import image from '../assets/crypto-image/image.png';
-import image2 from '../assets/crypto-image/image-2.png';
-import image3 from '../assets/crypto-image/image-3.png';
-import image4 from '../assets/crypto-image/image-4.png';
-import image5 from '../assets/crypto-image/image-5.png';
-import image1 from '../assets/crypto-image/image-1.png';
-import image6 from '../assets/crypto-image/image-6.png';
-import image7 from '../assets/crypto-image/image-7.png';
+import image from '../assets/crypto-image/Image.png';
+import image2 from '../assets/crypto-image/Image-2.png';
+import image3 from '../assets/crypto-image/Image-3.png';
+import image4 from '../assets/crypto-image/Image-4.png';
+import image5 from '../assets/crypto-image/Image-5.png';
+import image1 from '../assets/crypto-image/Image-1.png';
+import image6 from '../assets/crypto-image/Image-6.png';
+import image7 from '../assets/crypto-image/Image-7.png';
 
 const images = [image, image1, image2, image3, image4, image5, image6, image7];
 const duplicatedImages = [...images, ...images];
